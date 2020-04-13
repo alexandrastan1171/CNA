ea93d4d5824519c294c24ebceec77b617ac49c36
AlexandraS <alexandrastan1171@gmail.com>
2020-04-13 23:27:58 +0300

    Tema nr.1 - CNA

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcService1; //numele server-ului
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Va rugam introduceti numele: ");
+            name = Console.ReadLine();
+            var input = new HelloRequest { Name = name };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            await client.SayHelloAsync(input);
+        }
+    }
+}
